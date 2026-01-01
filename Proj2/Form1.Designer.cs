8a84fb0e697b3bfdbfa6980343091e6dbe526a12
Fatemeh-sr <Fatemeh.srvni8@gmail.com>
2026-01-01 23:50:59 +0330

    add files

---
+++ Proj2/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Proj2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(289, 527);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(481, 528);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Location = new System.Drawing.Point(593, 528);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Last";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Location = new System.Drawing.Point(142, 527);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 33);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Previous";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button5.Location = new System.Drawing.Point(30, 528);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(73, 33);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "First";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(305, 580);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 20);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "slide show";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox1.Location = new System.Drawing.Point(142, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(416, 420);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 636);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Proj2/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proj2
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] load = new PictureBox[20];
+        int count = 0;
+        int index = 0;
+        float zoomScale = 1.0f;
+        const float ZOOM_SPEED = 0.1f;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png";
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (var item in openFileDialog1.FileNames)
+                    count++;
+                if (count > 20)
+                    MessageBox.Show("حداکثر 20 تصویر میتوانید آپلود کنید");
+                for (int i = 0; i < count; i++)
+                {
+                    load[i] = new PictureBox();
+                    load[i].BorderStyle = BorderStyle.FixedSingle;
+                    load[i].SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+                    this.Controls.Add(load[i]);
+                    load[i].Image = Image.FromFile(openFileDialog1.FileNames[i]);
+                    load[i].Location = pictureBox1.Location;
+                    pictureBox1.Image = load[0].Image;
+                    button4.Enabled = false;
+                    button5.Enabled = false;
+
+
+                }
+            }
+
+            button1.Enabled = false;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (index <count-1)
+            {
+                index++;
+                ShowImage();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (index> 0)
+            {
+                index--;
+                ShowImage();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            index = 0;
+            ShowImage();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            index = count-1;
+            ShowImage();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                index = 0;
+                pictureBox1.Image = load[index].Image;
+
+                timer1.Interval = 3000;
+                timer1.Start();
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+            }
+            else
+            {
+                timer1.Stop();
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+
+                if (index > 0)
+                {
+                    button4.Enabled = true;
+                    button5.Enabled = true;
+                }
+                else
+                {
+                    button4.Enabled = false;
+                    button5.Enabled = false;
+                }
+
+                if (index < count-1)
+                {
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                }
+                else
+                {
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                }
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (count == 0) return;
+
+            index++;
+            if (index >= count)
+            {
+                index = 0;
+                timer1.Stop();
+                checkBox1.Checked = false;
+            }
+            pictureBox1.Image = load[index].Image;
+        }
+        private void ShowImage()
+        {
+            if (count == 0) return;
+
+            pictureBox1.Image = load[index].Image;
+
+            if (index > 0)
+                button4.Enabled = true;
+            else
+                button4.Enabled = false;
+
+            if (index > 0)
+                button5.Enabled = true;
+            else
+                button5.Enabled = false;
+
+            if (index < count-1)
+                button2.Enabled = true;
+            else
+                button2.Enabled = false;
+
+            if (index < count-1)
+                button3.Enabled = true;
+            else
+                button3.Enabled = false;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void ApplyZoom()
+        {
+            if (pictureBox1.Image == null) return;
+
+            pictureBox1.Width = (int)(pictureBox1.Image.Width * zoomScale);
+            pictureBox1.Height = (int)(pictureBox1.Image.Height * zoomScale);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                zoomScale += ZOOM_SPEED;
+                ApplyZoom();
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                zoomScale = Math.Max(0.5f, zoomScale - ZOOM_SPEED);
+                ApplyZoom();
+            }
+        }
+    }
+}
